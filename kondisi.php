47b4a0a2111a40b72dd5e612f43340c20d60955c
Agus Edy Cahyono <edyagusc@gmail.com>
2014-11-23 21:18:04 +0700

    Materi

---
+++ kondisi.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_POST[""])){
+	$lampu = $_POST["lampu"];
+
+echo "Warna lampu =>> ".$lampu;
+
+echo "<br><br>";
+if ($lampu=='Merah') {
+	echo 'Berhenti';
+}elseif($lampu=='Kuning'){
+	echo 'Hati hati';
+}else{
+	echo 'Jalan';
+}
+}
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<title></title>
+ 	<link rel="stylesheet" href="">
+ </head>
+ <body>
+ 	<form action="" method="POST">
+ 		<button type="submit" name="lampu" value="Merah">Merah</button>
+ 		<button type="submit" name="lampu" value="Kuning">Kuning</button>
+ 		<button type="submit" name="lampu" value="Hijau">Hjau</button>
+ 	</form>
+ </body>
+ </html>
